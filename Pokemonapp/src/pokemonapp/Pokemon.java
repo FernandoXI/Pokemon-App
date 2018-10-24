1611e9990d654d9763e94d3d9d1510f294d7bbf1
Luis Fernando <04fe1997@gmail.com>
2018-10-24 12:26:17 -0500

    clase agregada

---
+++ Pokemonapp/src/pokemonapp/Pokemon.java
@@ -0,0 +1,30 @@
+
+package pokemonapp;
+
+
+public class Pokemon 
+{
+    private String Nombre;
+    private int nivel;
+
+    public String getNombre() 
+    {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre)
+    {
+        this.Nombre = Nombre;
+    }
+
+    public int getNivel()
+    {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) 
+    {
+        this.nivel = nivel;
+    }
+    
+}
